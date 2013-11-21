561294a36fc408e3f1a8c539a0dac200ede4b60a
Neil Harvey <neil.harvey@hotmail.co.uk>
2013-11-21 13:38:51 +0000

    Added first two scenarios definitions.

---
+++ GildedRose/GildedRose.Tests/Features/UpdateInventory.feature
@@ -0,0 +1,14 @@
+﻿Feature: Update Inventory
+	As an inn keeper
+	I want the system to automatically update item quality and sell by dates
+	So that I can keep track of items
+
+Scenario: Quality reduces each day
+	Given an item with quality of 10
+	When the system updates the inventory
+	Then the item should have a quality of 9
+
+Scenario: Sell by date reduces each day
+	Given an item with a sell in of 10
+	When the system updates the inventory
+	Then the item should have a sell in of 9
---
+++ GildedRose/GildedRose.Tests/UpdateInventorySteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [Binding]
+    public class UpdateInventorySteps
+    {
+        [Given("an item with quality of (.*)")]
+        public void AnItemWithQualityOf(int quality)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When("the system updates the inventory")]
+        public void WhenTheSystemUpdatesTheInventory()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then("the item should have a quality of (.*)")]
+        public void ThenTheItemShouldHaveAQualityOf(int quality)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
